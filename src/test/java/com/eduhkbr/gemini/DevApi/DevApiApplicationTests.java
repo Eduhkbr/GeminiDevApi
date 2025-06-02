5822d01f984b05bbafa18671cf0aab190ed28e7b
Eduhkbr <47605719+Eduhkbr@users.noreply.github.com>
2025-06-02 12:48:53 +0000

    Adiciona perfil ativo "dev" aos testes de integração

--- src/test/java/com/eduhkbr/gemini/DevApi/DevApiApplicationTests.java
+++ src/test/java/com/eduhkbr/gemini/DevApi/DevApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.eduhkbr.gemini.DevApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class DevApiApplicationTests {
 
 	@Test
